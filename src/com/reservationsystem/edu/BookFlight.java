78c0ff2df052b88515df67400e068a7d3d8d3767
solislaura <79953943+solislaura@users.noreply.github.com>
2021-03-05 15:41:39 -0600

    Update BookFlight.java
    Made a few spelling corrections/suggestions.

--- src/com/reservationsystem/edu/BookFlight.java
+++ src/com/reservationsystem/edu/BookFlight.java
@@ -5,21 +5,21 @@ import java.util.Set;
 public class BookFlight {
 	private User user;
 	
-	private int iternaryNumber;
+	private int itineraryNumber;
 	private Flight flight;
 	
-	private Passanger passanger;
+	private Passenger passenger;
 	private Seat seat;
 	
-	public void bookFlight(Flight flight, Seat seat, Set<Passanger> passanger, User user) {
+	public void bookFlight(Flight flight, Seat seat, Set<Passenger> passenger, User user) {
 		
 	}
 	
-	public void editFlight(Flight flight, Seat seat, Set<Passanger> passanger, User user) {
+	public void editFlight(Flight flight, Seat seat, Set<Passenger> passenger, User user) {
 		
 	}
 
-	public void cancelFlight(int iternaryNumber) {
+	public void cancelFlight(int itineraryNumber) {
 		
 	}
 }
